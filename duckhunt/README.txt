eedc2ee82317c61a17efe6e18f7a2bc90de050b5
Kayden Mimmack <mimmackattack@gmail.com>
2015-10-13 01:00:48 -0400

    Edited game.js file and created README.txt. Lab is complete.

---
+++ duckhunt/README.txt
@@ -0,0 +1,9 @@
+Katherine Mimmack Duck Hunt game:
+
+1. The images load correctly on the screen and in the proper locations.
+   What has not been implemented is the animation for the ducks.
+2. I did not work with others on the project, but I did draw heavily from the
+   following sources:
+   https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+   https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+3. I spent approximately 1 hour completing the assignment.
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,21 @@
-// Replace this with your code...
+// Katherine Mimmack 10/13/2015
+
+function init() {
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext('2d');
+
+	// draws the background
+	var bg = new Image();
+	bg.src = "duckhunt-background.gif";
+	bg.onload = function() {
+		ctx.drawImage(bg, 0, 0, 800, 600);
+	}
+
+	// draws ducks
+	var duck = new Image();
+	duck.src = "duckhunt_various_sheet.png"
+	duck.onload = function() {
+		ctx.drawImage(duck, 0, 117, 40, 35, 160, 50, 100, 90); // Duck 1
+		ctx.drawImage(duck, 169, 121, 40, 35, 320, 100, 100, 90); // Duck 2
+	}
+}

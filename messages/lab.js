aa66ce6f966cb98076f8dc4485e8f910919b0043
Kayden Mimmack <mimmackattack@gmail.com>
2015-10-22 23:06:50 -0400

    Earlier push did not update lab.js, second attempt at update

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here...
+req = new XMLHttpRequest();
+
+function parse()
+{
+	req.open("GET", "data.json", true);
+	req.onreadystatechange = function getdata()
+	{
+		if (req.readyState == 4)
+		{
+			console.log("Got info: success");
+			data = req.responseText;
+			msgs = JSON.parse(data);
+			elem = document.getElementById("messages");
+			for (i = 0; i < msgs.length; i++)
+			{
+				elem.innerHTML += "<p>" + msgs[i]["content"] + "</p>"
+				+ '<p><div id="user">' + msgs[i]["username"] + "</div></p>";
+			}
+		}
+
+		else
+			console.log("Not yet successful");
+	};
+
+	req.send(null);
+}
